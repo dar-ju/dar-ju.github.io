3b62673a2e5751b35ed21be470fb6be3d7f45448
dar-ju <166850470+dar-ju@users.noreply.github.com>
2024-12-10 21:39:42 +0300

    FM_09_newsletter

--- FM_09_newsletter/js/main.js
+++ FM_09_newsletter/js/main.js
@@ -1,15 +1,16 @@
+const newsletter = document.querySelector('.newsletter');
 const form = document.getElementById('subscribe');
-const newsletter = document.querySelector('.newsletter')
-const inputField = document.querySelector('.newsletter__form-input')
-const errorNotice = document.querySelector('.newsletter__form-notice')
-const successModal = document.querySelector('.success__container')
+const inputField = document.getElementById('subscribe-email-field');
 const submitButton = document.getElementById('subscribe-button');
+const errorNotice = document.querySelector('.newsletter__form-notice');
+const successModal = document.querySelector('.success__container');
+const successDescription = document.querySelector('.success__description');
 const successButton = document.getElementById('success-button');
 
 // email check function
 const emailCheck = () => {
   const inputText = inputField.value;
-  const regular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  const regular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!regular.test(inputText)) {
     inputField.classList.add('newsletter__form-input_error');
     errorNotice.style.opacity = '1';
@@ -24,14 +25,15 @@ const emailCheck = () => {
 
 // email check when leave input field
 inputField.addEventListener('blur', () => {
-  emailCheck()
+  if (inputField.value) emailCheck();
 })
 
 // submit form
 form.addEventListener('submit', (event) => {
-	event.preventDefault();
+  event.preventDefault();
   emailCheck()
   if (inputField.value) {
+    successDescription.innerHTML = `A&nbsp;confirmation email has been sent to <span class="success__email">${inputField.value}</span>. Please open it&nbsp;and click the button inside to&nbsp;confirm your subscription.`;
     newsletter.style.display = 'none';
     successModal.style.animation = 'modal-fadeIn 0.3s ease-in';
     successModal.style.display = 'block';
@@ -41,7 +43,7 @@ form.addEventListener('submit', (event) => {
 
 // close success modal window
 successButton.addEventListener('click', (event) => {
-	event.preventDefault();
+  event.preventDefault();
   successModal.style.animation = 'modal-fadeOut 0.2s ease-in';
   setTimeout(() => {
     newsletter.style.display = 'block';
