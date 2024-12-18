e2ac8d60785bda08fed546406d41f025f3102039
Андрей Немков <nemkov@Onlinetrade.local>
2024-12-18 12:40:30 +0300

    FM_11_tip-calculator

--- FM_11_tip-calculator/main.js
+++ FM_11_tip-calculator/main.js
@@ -1,36 +1,33 @@
-const allFields = document.querySelector('.tip-calculator__data')
-const billTotal = document.querySelector('.tip-calculator__input_bill')
-const tipVariations = document.querySelectorAll('.tip-calculator__tip-list li button')
-const numberOfPeople = document.querySelector('.tip-calculator__input_person')
+const allFields = document.querySelector('.splitter__data')
+const billTotal = document.querySelector('.splitter__input_bill')
+const tipVariations = document.querySelectorAll('.splitter__tip-list li button')
+const numberOfPeople = document.querySelector('.splitter__input_person')
 // Tip fields
-const customButton = document.querySelector('.tip-calculator__tip-button-block')
-const customLiField = document.querySelector('.tip-calculator__tip-item_custom-field')
-const customField = document.querySelector('.tip-calculator__input_tip')
-const warningMessage = document.querySelector('.tip-calculator__warning')
+const customButton = document.querySelector('.splitter__tip-button-block')
+const customLiField = document.querySelector('.splitter__tip-item_custom-field')
+const customField = document.querySelector('.splitter__input_tip')
+const warningMessage = document.querySelector('.splitter__warning')
 let tipValue
 // Result fields
 const resultAmount = document.getElementById('resultAmount')
 const resultTotal = document.getElementById('resultTotal')
 // Reset
 const resetButton = document.getElementById('reset')
-const addResetListener = () => { resetButton.classList.add('tip-calculator__main-button_hovered') }
-const removeResetListener = () => { resetButton.classList.remove('tip-calculator__main-button_hovered') }
 
 // Setup default focus
 billTotal.focus();
 
 // Prevent form submit
 allFields.addEventListener('submit', (event) => {
-  event.preventDefault();
+  event.preventDefault()
 })
 
 // Fields listener
 allFields.addEventListener('change', (event) => {
-  event.preventDefault();
   if (event.target === customField) tipValue = customField.value
   if (event.target === numberOfPeople && Number(numberOfPeople.value)) {
     warningMessage.style.display = 'none'
-    numberOfPeople.classList.remove('tip-calculator__input_outline')
+    numberOfPeople.classList.remove('splitter__input_outline')
   }
   getResult()
   peopleCheck()
@@ -39,7 +36,7 @@ allFields.addEventListener('change', (event) => {
 // Button color reset
 function colorReset() {
   tipVariations.forEach(element => {
-    element.classList.remove('tip-calculator__tip-item_pushed')
+    element.classList.remove('splitter__tip-item_pushed')
   })
 }
 
@@ -51,13 +48,13 @@ tipVariations.forEach(element => {
     if (event.target.textContent === 'Custom') {
       customButton.style.display = 'none'
       customLiField.style.display = 'initial'
-      customField.focus();
+      customField.focus()
     } else if (!tip) return
     else {
       customField.value = ''
       customLiField.style.display = 'none'
       customButton.style.display = 'initial'
-      event.target.classList.add('tip-calculator__tip-item_pushed')
+      event.target.classList.add('splitter__tip-item_pushed')
       tipValue = Number(tip)
     }
     getResult()
@@ -66,9 +63,9 @@ tipVariations.forEach(element => {
 
 // Number of people check
 function peopleCheck() {
-  if (!numberOfPeople.value) {
+  if (!Number(numberOfPeople.value)) {
     warningMessage.style.display = 'block'
-    numberOfPeople.classList.add('tip-calculator__input_outline')
+    numberOfPeople.classList.add('splitter__input_outline')
   }
 }
 
@@ -77,7 +74,7 @@ function formatData(value) {
   return new Intl.NumberFormat("en", {style: "currency", currency: "USD"}).format(value);
 }
 
-// Tip calculations
+// Tip calculations & get result
 function getResult() {
   if (!tipValue) tipValue = customField.value
   const amount = (billTotal.value / numberOfPeople.value) * (tipValue / 100)
@@ -85,17 +82,19 @@ function getResult() {
   if (amount && amount !== Infinity) {
     resultAmount.textContent = formatData(amount)
     resultTotal.textContent = formatData(total)
-    resetButton.removeAttribute('disabled');
-    resetButton.classList.add('tip-calculator__main-button_enabled')
-    resetButton.addEventListener('mouseover', () => {
-      addResetListener()
-    });
-
-    resetButton.addEventListener('mouseout', () => {
-      removeResetListener()
-    });
+    resetButton.removeAttribute('disabled')
+    resetButton.classList.add('splitter__main-button_enabled')
+    resetButton.addEventListener('mouseover', addHover)
+    resetButton.addEventListener('mouseout', removeHover)
+  } else {
+    resultAmount.textContent = formatData(0)
+    resultTotal.textContent = formatData(0)
   }
 }
+
+// Reset button hover listeners
+const addHover = () => resetButton.classList.add('splitter__main-button_hovered');
+const removeHover = () => resetButton.classList.remove('splitter__main-button_hovered');
 
 // Reset form
 resetButton.addEventListener('click', () => {
@@ -110,13 +109,9 @@ resetButton.addEventListener('click', () => {
     customButton.style.display = 'initial'
   }
   resetButton.setAttribute('disabled', '')
-  resetButton.classList.remove('tip-calculator__main-button_enabled')
-  resetButton.removeEventListener('mouseover', () => {
-    addResetListener()
-  });
-  resetButton.removeEventListener('mouseout', () => {
-    removeResetListener()
-  });
+  resetButton.classList.remove('splitter__main-button_enabled')
+  resetButton.classList.remove('splitter__main-button_hovered')
+  resetButton.removeEventListener('mouseover', addHover);
+  resetButton.removeEventListener('mouseout', removeHover);
   billTotal.focus();
 })
-
