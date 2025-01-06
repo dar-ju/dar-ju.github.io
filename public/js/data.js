45cd1e2f019f5292a49c02bd021fa4ecf90de20c
dar-ju <166850470+dar-ju@users.noreply.github.com>
2025-01-06 16:04:14 +0300

    FM page

---
+++ public/js/data.js
@@ -0,0 +1,225 @@
+export const data = [
+  {
+    "news-homepage": {
+      "en": {
+        "title": "News homepage",
+        "description": "This news homepage was a great opportunity to practice my CSS Grid skills. There were many challenging decisions and many learning opportunities!",
+      },
+      "ru": {
+        "title": "Домашняя страница сайта новостей",
+        "description": "Эта новостная домашняя страница стала отличной возможностью попрактиковать свои навыки CSS Grid. Было много сложных решений и много возможностей для обучения!",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/news-homepage-8JaK1hJGlY"
+    }
+  },
+  {
+    "form": {
+      "en": {
+        "title": "Contact form",
+        "description": "Creating accessible forms is an important task for FrontEnd developers. This task helped to practice creating a form with multiple input types and validation.",
+      },
+      "ru": {
+        "title": "Контактная форма",
+        "description": "Создание доступных форм — важная задача для FrontEnd разработчиков. Эта задача помогла попрактиковаться в создании формы с несколькими типами ввода и проверкой.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/contact-form-BEClvqbx_z"
+    }
+  },
+  {
+    "accordion": {
+      "en": {
+        "title": "FAQ accordion",
+        "description": "This assignment involved creating a FAQ accordion, a common interface template, which provided a great opportunity for practice.",
+      },
+      "ru": {
+        "title": "FAQ аккордеон",
+        "description": "В этом задании предполагается создание аккордеона FAQ, распространенного шаблона интерфейса, что дало прекрасную возможность для практики.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/faq-accordion-spnKX4fBKt"
+    }
+  },
+  {
+    "rating": {
+      "en": {
+        "title": "Interactive rating component",
+        "description": "This is a nice, small project to practice handling user interactions and updating the DOM. Perfect for anyone who has learned the basics of JavaScript!",
+      },
+      "ru": {
+        "title": "Интерактивный компонент рейтинга",
+        "description": "Это хороший, небольшой проект для практики обработки взаимодействия с пользователем и обновления DOM. Идеально подходит для тех, кто изучил основы JavaScript!",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/rating-component-d0p1yN7S37"
+    }
+  },
+  {
+    "tip-calculator": {
+      "en": {
+        "title": "Tip calculator app",
+        "description": "A simple app designed to help grasp JavaScript fundamentals, featuring calculator functionality as a practical test.",
+      },
+      "ru": {
+        "title": "Приложение для расчета чаевых",
+        "description": "Простое приложение, призванное помочь освоить основы JavaScript, включающее в себя функциональность калькулятора в качестве практического теста.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/tip-calculator-iASjBMcYHy"
+    }
+  },
+  {
+    "time-tracking": {
+      "en": {
+        "title": "Time tracking dashboard",
+        "description": "An ideal opportunity to practice CSS Grid skills, with the added option of using a provided JSON data file to enhance data-handling abilities.",
+      },
+      "ru": {
+        "title": "Панель отслеживаниея времени",
+        "description": "Идеальная возможность попрактиковаться в работе с CSS Grid с дополнительной возможностью использования предоставленного файла JSON для улучшения навыка обработки данных.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/time-tracking-dash-ZZ0twPiIzt"
+    }
+  },
+  {
+    "newsletter": {
+      "en": {
+        "title": "Newsletter sign-up form with success message",
+        "description": "This will test your skills with basic form structure, validation, and submission. The success state will also be an excellent opportunity to work with DOM manipulation.",
+      },
+      "ru": {
+        "title": "Форма подписки на рассылку с сообщением об успешном завершении",
+        "description": "Задание проверяет навыки работы с базовой структурой формы, проверкой и отправкой. Состояние успеха также является отличной возможностью поработать с DOM.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/newsletter-sign-up-form-XjJJFKYwpA"
+    }
+  },
+  {
+    "js-aricle": {
+      "en": {
+        "title": "Article preview component",
+        "description": "Task focuses on improving layout skills with an article preview component while allowing creative exploration of animations for sharing icons.",
+      },
+      "ru": {
+        "title": "Компонент статьи",
+        "description": "Задача направлена ​​на улучшение навыков верстки с помощью компонента статьи, а также на творческий подход к созданию анимаций для иконок профиля.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/article-card-aj796wU0fb"
+    }
+  },
+  {
+    "testimonials": {
+      "en": {
+        "title": "Testimonials grid section",
+        "description": "The challenge provided an opportunity to practice and test CSS Grid skills, highlighting its versatility and value as a powerful tool in CSS design.",
+      },
+      "ru": {
+        "title": "Раздел отзывов по сетке",
+        "description": "Задание предоставило возможность попрактиковаться и проверить навыки работы с CSS Grid, подчеркивая их универсальность и ценность как мощного инструмента в дизайне CSS.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/testimonials-grid-page-2UN6G5TJDl"
+    }
+  },
+  {
+    "four-card": {
+      "en": {
+        "title": "Four card feature section",
+        "description": "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+      },
+      "ru": {
+        "title": "Раздел с четырьмя секциями",
+        "description": "Хорошая задача по работе с макетом. Это проверит любого, кто новичок в многоколоночных и адаптивных макетах.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/four-card-adaptive-zWDhvnCGKe"
+    }
+  },
+  {
+    "product": {
+      "en": {
+        "title": "Product card",
+        "description": "This HTML & CSS-only challenge will be perfect starting to build responsive projects.",
+      },
+      "ru": {
+        "title": "Карточка товара",
+        "description": "Это задание, посвященное только HTML и CSS - идеальное начало для создания адаптивных проектов.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/product-card-without-figma-ZzvSFsAS3r"
+    }
+  },
+  {
+    "recipe": {
+      "en": {
+        "title": "Recipe page",
+        "description": "In this challenge emphasizes writing semantic HTML by selecting the most suitable elements for each piece of content.",
+      },
+      "ru": {
+        "title": "Страница рецептов",
+        "description": "В этом задании особое внимание уделяется написанию семантического HTML-кода путем выбора наиболее подходящих элементов для каждой части контента.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/recipe-page-BCT67jz4_O"
+    }
+  },
+  {
+    "profile": {
+      "en": {
+        "title": "Social links profile",
+        "description": "This small project involves creating a social link-sharing profile. It can be personalized and used to share all social profiles.",
+      },
+      "ru": {
+        "title": "Профиль ссылок на социальные сети",
+        "description": "Этот небольшой проект включает в себя создание профиля для обмена социальными ссылками. Его можно персонализировать и использовать для обмена всеми социальными профилями.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/flexing-social-links-profile-bNmpzr6YpL"
+    }
+  },
+  {
+    "card": {
+      "en": {
+        "title": "Blog preview card",
+        "description": "This HTML & CSS-only challenge is a perfect project for beginners getting up to speed with HTML and CSS fundamentals, like HTML structure and the box model.",
+      },
+      "ru": {
+        "title": "Карточка блога",
+        "description": "Это задание, посвященное только HTML и CSS, идеально подойдет новичкам, которые хотят быстро освоить основы HTML и CSS, такие как структура HTML и блочная модель.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/blog-preview-card-1Vl5ErQuld"
+    }
+  },
+  {
+    "qr": {
+      "en": {
+        "title": "QR code component",
+        "description": "This is a great introductory example for working with HTML and CSS. The card layout remains fixed, making it ideal for those who have not yet learned about building responsive layouts.",
+      },
+      "ru": {
+        "title": "Компонент QR-кода",
+        "description": "Отличный вводный пример для работы с HTML и CSS. Макет карточки остается фиксированным, что делает его идеальным для тех, кто еще не научился создавать адаптивные макеты.",
+      },
+      "urlFm": "https://www.frontendmentor.io/solutions/fm01qr-E_iuILoOSx"
+    }
+  },
+  {
+    "commonLang": {
+      "en": {
+        "title": "Solutions",
+        "heroDescription1": "Frontend Mentor is an online platform to help developers improve their frontend coding skills. It offers real-world projects, from simple layouts to advanced apps using JavaScript frameworks.",
+        "heroDescription2": "Users choose challenges, receive design files, and build projects that mimic real-world scenarios. Completed projects can be&nbsp;uploaded for feedback from the community, helping refine skills and follow modern standards.",
+        "heroDescription3": "Frontend Mentor is great for beginners and experienced developers to grow their portfolios and gain hands-on experience. The active community fosters collaboration and learning, bridging the gap between theory and real-world application.",
+        "heroDescription4": "<strong>For me personally</strong>, this platform is a useful tool for improving my skills. At first glance, the tasks seem easy, but during development, a large number of details become visible. It is the details that are one of the most important parts of development, and by focusing on them, you can get a truly high-quality product.",
+        "subtitle1": "My Latest",
+        "subtitle2": "All Solutions",
+        "btnLive": "live site",
+        "btnFm": "FM page",
+        "thank": "I hope you enjoyed it",
+      },
+      "ru": {
+        "title": "Решения",
+        "heroDescription1": "Frontend Mentor — это онлайн-платформа, помогающая разработчикам улучшить свои Frontend навыки. Она предлагает реальные проекты, от простых макетов до сложных приложений с использованием фреймворков JavaScript.",
+        "heroDescription2": "Пользователи выбирают задачи, получают файлы дизайна и создают проекты, имитирующие реальные задачи. Завершенные проекты можно загрузить для получения отзывов от сообщества, что помогает оттачивать навыки и следовать современным стандартам.",
+        "heroDescription3": "Frontend Mentor отлично подходит для начинающих и опытных разработчиков, чтобы расширить свои портфолио и получить практический опыт. Активное сообщество способствует сотрудничеству и обучению, сокращая разрыв между теорией и практикой.",
+        "heroDescription4": "<strong>Лично для меня</strong> эта платформа является полезным инструментом для повышения своих навыков. На первый взгляд задания кажутся легкими, но в процессе разработки становится видно большое количество деталей. Именно детали являются одной из важнейших частей разработки, и, сосредоточившись на них, можно получить действительно качественный продукт.",
+        "subtitle1": "Мое Последнее",
+        "subtitle2": "Все Решения",
+        "btnLive": "страница",
+        "btnFm": "страница FM",
+        "thank": "Надеюсь вам понравилось",
+      }
+    }
+  }
+]
---
+++ public/js/main.js
@@ -0,0 +1,129 @@
+import { data } from "./data.js";
+
+const langEng = document.getElementById('langEng')
+const langRus = document.getElementById('langRus')
+
+let lang = 'en'
+
+langEng.addEventListener('click', (event) => {
+  event.preventDefault()
+  langToggle()
+})
+langRus.addEventListener('click', (event) => {
+  event.preventDefault()
+  langToggle()
+})
+
+function langToggle() {
+  if (lang === 'en') {
+    langRus.classList.add('selected')
+    langEng.classList.remove('selected')
+    lang = 'ru'
+  } else {
+    langEng.classList.add('selected')
+    langRus.classList.remove('selected')
+    lang = 'en'
+  }
+  loadData()
+}
+
+function loadData() {
+  data.forEach(async (item) => await createSectionCards(item))
+}
+loadData()
+
+async function checkImage(path1, path2) {
+  try {
+    const response = await fetch(path1)
+    if (response.ok) {
+      return path1
+    } else {
+      return path2
+    }
+  } catch (error) {
+    return path2
+  }
+}
+
+async function createSectionCards(item) {
+  const allGroup = document.querySelector('.all__group')
+  const latestCard = document.querySelector('.latest__card')
+  allGroup.innerHTML = ''
+  latestCard.innerHTML = ''
+
+  const taskName = Object.keys(item)[0]
+  if (taskName === 'commonLang') return
+
+  const findItemIndex = data.findIndex(el => el[taskName] !== undefined)
+
+  let taskId = data.length - findItemIndex - 1
+  taskId = taskId > 9 ? taskId : '0' + taskId
+
+  function getImagePath() {
+    return checkImage(`./FM_${taskId}_${taskName}/design/desktop-preview.jpg`, `./FM_${taskId}_${taskName}/design/desktop-design.jpg`)
+  }
+
+  const image = await getImagePath()
+  const title = item[taskName][lang].title
+  const description = item[taskName][lang].description
+  const urlLive = `https://dar-ju.github.io/FM_${taskId}_${taskName}/`
+  const urlFm = item[taskName].urlFm
+  const urlGit = `https://github.com/dar-ju/dar-ju.github.io/tree/main/FM_${taskId}_${taskName}`
+
+  const cardAll = `
+    <li class="all__card">
+      <img class="lazyload all__image" data-src="${image}" alt="${taskName} challenge image">
+      <div class="all__wrapper">
+        <div class="all__about">
+          <h3 class="all__name">${title}</h3>
+          <p class="all__description">${description}</p>
+        </div>
+        <div class="all__button-group">
+          <a class="btn link all__btn live-btn" href="${urlLive}">Live site</a>
+          <a class="btn link all__btn fm-btn" href="${urlFm}">FM page</a>
+          <a class="btn link all__btn" href="${urlGit}">Github</a>
+        </div>
+      </div>
+    </li>
+  `
+
+  const cardLatest = `
+    <div class="latest__about">
+      <h3 class="latest__name">${title}</h3>
+      <p>${description}</p>
+      <div class="latest__button-group">
+        <a class="btn link live-btn" href="${urlLive}">Live site</a>
+        <a class="btn link fm-btn" href="${urlFm}">FM page</a>
+        <a class="btn link" href="${urlGit}">Github</a>
+      </div>
+    </div>
+    <img class="lazyload latest__img" data-src="${image}" alt="${taskName} challenge image">
+  `
+
+  if (data.length - findItemIndex === data.length) latestCard.insertAdjacentHTML("beforeend", cardLatest)
+  else allGroup.insertAdjacentHTML("beforeend", cardAll)
+
+  loadLang()
+  lazyload()
+}
+
+function loadLang() {
+  const langItem = data[data.length - 1].commonLang[lang]
+  document.getElementById('menuLatest').textContent = langItem.subtitle1
+  document.getElementById('menuAll').textContent = langItem.subtitle2
+  document.querySelector('.hero__title-background').textContent = langItem.title
+  document.querySelector('.latest__subtitle').textContent = langItem.subtitle1
+  document.querySelector('.all__subtitle').textContent = langItem.subtitle2
+  document.querySelectorAll('.live-btn').forEach(btn => {
+    btn.textContent = langItem.btnLive;
+  });
+  document.querySelectorAll('.fm-btn').forEach(btn => {
+    btn.textContent = langItem.btnFm;
+  });
+  for (let i = 1; i <= 4; i++) {
+    document.querySelector(`.hero__description p:nth-of-type(${i})`).innerHTML = langItem[`heroDescription${i}`]
+  }
+  document.querySelector('.footer__thank').textContent = langItem.thank
+}
+
+document.querySelector('.footer__copy').textContent = `darju ${new Date().getFullYear()}`
