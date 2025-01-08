021460c5f7ed65ab152516b0e456c57fe5b2c5e6
dar-ju <166850470+dar-ju@users.noreply.github.com>
2025-01-08 22:21:50 +0300

    FM_16_ecommerce-product

---
+++ FM_16_ecommerce-product/FM_16_project/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeaderSection from '@/components/HeaderSection.vue'
+import ProductPage from '@/views/ProductPage.vue'
+</script>
+
+<template>
+  <header class="header section">
+    <HeaderSection />
+  </header>
+  <ProductPage />
+</template>
+
+<style scoped></style>
---
+++ FM_16_ecommerce-product/FM_16_project/src/views/ProductPage.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>HELLO</h1>
+  </main>
+</template>
+
+<style></style>
