4c06c2c4d8b0fe0df6762ee4ccfa4f330415524e
dar-ju <7061630@gmail.com>
2025-01-21 22:16:31 +0300

    FM_19_room-homepage

--- FM_19_room-homepage/js/main.js
+++ FM_19_room-homepage/js/main.js
@@ -3,72 +3,46 @@ import { data } from "./data.js";
 let firstImageNumber = 1
 
 const imagesGroup = document.querySelector('.carousel__images')
+const title = document.querySelector('.carousel__title')
+const descr = document.querySelector('.carousel__description')
 const prevBtn = document.getElementById('prevBtn')
 const nextBtn = document.getElementById('nextBtn')
-const dataLength = Object.keys(data).length
-const imagesIdArr = []
-
-// const imageOrder = (number) => {
-//   if (number === -1)
-//   if (firstImageNumber - number === 0 || firstImageNumber + number > dataLength) return dataLength
-//   else return firstImageNumber + number
-// }
-
-const imagesToLoad = (item) => {
-  imagesIdArr.push(firstImageNumber - 1 === 0 ? dataLength : firstImageNumber - 1)
-  imagesIdArr.push(firstImageNumber)
-  imagesIdArr.push(firstImageNumber + 1 > dataLength ? 1 : firstImageNumber + 1)
-}
-imagesToLoad()
-
-
-
 
 function imageAdd(item) {
   const index = data.findIndex(el => el.id === item)
   return `
-    <picture class="carousel__image-wrapper" carouselId="${item}">
+    <picture class="carousel__image-wrapper" id="${item}">
       <source media="(max-width: 375px)" srcset=${data[index].urlMobile}>
       <img class="carousel__image" src=${data[index].urlDesktop}
-        alt="Discover innovative ways to decorate image">
+        alt="${data[index].title}">
     </picture>
   `
 }
+imagesGroup.insertAdjacentHTML("beforeend", imageAdd(firstImageNumber))
 
-function imagesFill() {
-  imagesIdArr.forEach(item => {
-    imagesGroup.insertAdjacentHTML("beforeend", imageAdd(item))
-  });
-}
-imagesFill()
-
-function animationMovement(className) {
-  const images = document.querySelectorAll('.carousel__image-wrapper')
-  images.forEach(element => {
-    element.classList.add(className)
-  });
+function imageId(id) {
+  const totalImages = Object.keys(data).length
+  if (id > totalImages) return 1
+  if (id === 0) return totalImages
+  return id
 }
 
-prevBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  animationMovement('prevAnimation')
-  setTimeout(() => {
-    const carouselId = imagesGroup.children[imagesGroup.children.length - 1].getAttribute("carouselId")
-    imagesGroup.children[imagesGroup.children.length - 1].remove();
-    imagesGroup.insertAdjacentHTML("afterBegin", imageAdd(carouselId - 1))
-    // imagesGroup.insertAdjacentHTML("beforeend", imageAdd(carouselId - 1))
-  }, 1000);
-})
-// parent.style.display = 'none';
-//     parent.style.display = 'block';
+function handleButtonClick(direction) {
+  //image animation
+  const currentImage = document.querySelector('.carousel__image-wrapper');
+  const newImageId = imageId(Number(currentImage.id) + direction);
+  imagesGroup.insertAdjacentHTML("afterbegin", imageAdd(newImageId));
+  const images = document.querySelectorAll('.carousel__image-wrapper');
+  gsap.to(images[1], { duration: 1, x: 840 * direction, ease: "power3.inOut" });
+  setTimeout(() => { images[1].remove() }, 1000);
+  // title and descr animation
+  const timeline = gsap.timeline();
+  timeline
+    .to([title, descr], { duration: 0.5, opacity: 0 })
+    .call(() => { title.textContent = data[newImageId - 1].title })
+    .call(() => { descr.textContent = data[newImageId - 1].description })
+    .to([title, descr], { duration: 0.5, opacity: 1 })
+}
 
-nextBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  animationMovement('nextAnimation')
-  setTimeout(() => {
-    const carouselId = imagesGroup.children[0].getAttribute("carouselId")
-    imagesGroup.children[0].remove();
-    imagesGroup.insertAdjacentHTML("afterBegin", imageAdd(carouselId + 1))
-    // imagesGroup.insertAdjacentHTML("beforeend", imageAdd(carouselId - 1))
-  }, 1000);
-})
+prevBtn.addEventListener('click', () => handleButtonClick(-1));
+nextBtn.addEventListener('click', () => handleButtonClick(1));
