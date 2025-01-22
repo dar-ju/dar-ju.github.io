6b11bd3c1dfda3093fd0e7d6d5410e54f3658756
Андрей Немков <nemkov@Onlinetrade.local>
2025-01-22 16:31:54 +0300

    FM_19_room-homepage final

--- FM_19_room-homepage/js/main.js
+++ FM_19_room-homepage/js/main.js
@@ -2,6 +2,21 @@ import { data } from "./data.js";
 
 let firstImageNumber = 1
 
+// greet animation
+gsap.to('.greet__text', { opacity: 1, duration: 2, delay: 0.5 })
+gsap.to('.greet__logo', { opacity: 1, duration: 2, delay: 2 })
+const lines = document.querySelectorAll('.greet__line');
+gsap.to(".greet li", {
+  x: '100%',
+  stagger: 0.2,
+  duration: 1,
+  ease: "power3.out",
+  delay: 3.5,
+  onComplete: () => {
+    gsap.to(".greet", { display: 'none' });
+  }
+});
+
 const imagesGroup = document.querySelector('.carousel__images')
 const title = document.querySelector('.carousel__title')
 const descr = document.querySelector('.carousel__description')
@@ -33,7 +48,7 @@ function handleButtonClick(direction) {
   const newImageId = imageId(Number(currentImage.id) + direction);
   imagesGroup.insertAdjacentHTML("afterbegin", imageAdd(newImageId));
   const images = document.querySelectorAll('.carousel__image-wrapper');
-  gsap.to(images[1], { duration: 1, x: 840 * direction, ease: "power3.inOut" });
+  gsap.to(images[1], { duration: 1, x: getXValue(direction), ease: "power3.inOut" });
   setTimeout(() => { images[1].remove() }, 1000);
   // title and descr animation
   const timeline = gsap.timeline();
@@ -42,6 +57,11 @@ function handleButtonClick(direction) {
     .call(() => { title.textContent = data[newImageId - 1].title })
     .call(() => { descr.textContent = data[newImageId - 1].description })
     .to([title, descr], { duration: 0.5, opacity: 1 })
+  gsap.to('.carousel__btn svg', { duration: 0.3, x: 10, repeat: 1, yoyo: true, ease: 'power1.inOut' });
+}
+
+function getXValue(direction) {
+  return direction === 1 ? '100%' : '-100%';
 }
 
 prevBtn.addEventListener('click', () => handleButtonClick(-1));
