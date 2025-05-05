547ac58b998237410d6992d421de369134fbd631
dar-ju <7061630@gmail.com>
2025-05-05 09:53:32 +0300

    Update server.js

--- FM_29_todo-app/backend/server.js
+++ FM_29_todo-app/backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -12,11 +13,17 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Mongo connected'))
   .catch(err => console.error(err))
 
-const Post = mongoose.model('Post', { title: String, content: String })
+const Todo = mongoose.model('Todo', { title: String, content: String }, 'todos');
 
-app.get('/api/posts', async (req, res) => {
-  const posts = await Post.find()
-  res.json(posts)
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`)
 })
-
-app.listen(process.env.PORT || 3000)
