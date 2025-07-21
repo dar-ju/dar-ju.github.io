cc2113fa3706f8f12c94741cbaaea0b5b0229dbb
dar-ju <7061630@gmail.com>
2025-07-21 11:20:40 +0300

    added: average mood

--- FM_32_mood-tracking/vue-project/src/components/dashboard/StatAverageMood.vue
+++ FM_32_mood-tracking/vue-project/src/components/dashboard/StatAverageMood.vue
@@ -1,21 +1,89 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useDataStore } from '@/stores/moodData'
 import { useMoodMap } from '@/composables/useMoodMap'
 const { moodMap, sleepMap } = useMoodMap()
 
 const mood = useDataStore()
+
+const CHECKINS = 5
+
+const averageData = ref({
+  titleImg: null,
+  title: 'Keep tracking!',
+  arrowImg: null,
+  descr: `Log ${CHECKINS} check-ins to see your average mood.`,
+  bckgColor: 'var(--blue-100)',
+})
+
+watch(
+  () => mood.data,
+  () => {
+    // console.log(Object.values(mood.data.moodEntries.mood))
+    // console.log(mood.data.moodEntries.map((item) => item.mood))
+    const moods = mood.data.moodEntries.map((item) => item.mood)
+    if (moods.length >= CHECKINS) {
+      const lastNMoods = moods.slice(-CHECKINS)
+      const average = Math.round(lastNMoods.reduce((acc, val) => acc + val, 0) / CHECKINS)
+      averageData.value.title = moodMap[average].title
+      averageData.value.titleImg = moodMap[average].img
+      averageData.value.bckgColor = moodMap[average].color
+      console.log(lastNMoods)
+      console.log(average)
+      if (moods.length >= CHECKINS && moods.length < CHECKINS * 2) {
+        averageData.value.descr = `Log another ${CHECKINS} check‑ins to see your progress mood.`
+      } else {
+        const lastPrevNMoods = moods.slice(-CHECKINS * 2, -CHECKINS)
+        const averagePrev = Math.round(lastPrevNMoods.reduce((acc, val) => acc + val, 0) / CHECKINS)
+        if (average > averagePrev) {
+          averageData.value.descr = `Increase from the previous ${CHECKINS} check‑ins`
+          averageData.value.arrowImg = '/assets/images/icon-trend-increase.svg'
+        }
+        if (average < averagePrev) {
+          averageData.value.descr = `Decrease from the previous ${CHECKINS} check‑ins`
+          averageData.value.arrowImg = '/assets/images/icon-trend-decrease.svg'
+        }
+        if (average === averagePrev) {
+          averageData.value.descr = `Same as the previous ${CHECKINS} check‑ins`
+          averageData.value.arrowImg = '/assets/images/icon-trend-same.svg'
+        }
+      }
+    }
+  },
+)
 </script>
 
 <template>
   <div class="stat__average-wrapper stat__average-mood">
     <div class="stat__average-title-block">
       <h2 class="stat__average-title">Average Mood</h2>
-      <span class="stat__average-note">(Last 5 Check-ins)</span>
+      <span class="stat__average-note">{{ `(Last ${CHECKINS} Check-ins)` }}</span>
     </div>
-    <div class="stat__average-block">
-      <h3 class="stat__average-info-title">Keep tracking!</h3>
-      <p class="stat__average-info-descr">Log 5 check-ins to see your average mood.</p>
+    <div class="stat__average-block" :style="{ backgroundColor: averageData.bckgColor }">
+      <div class="stat__average-title-block">
+        <img
+          v-if="averageData.titleImg"
+          class="stat__average-img"
+          :src="averageData.titleImg"
+          alt=""
+          width="24"
+          height="24"
+        />
+        <h3 class="stat__average-info-title">{{ averageData.title }}</h3>
+      </div>
+      <div class="stat__average-descr-block">
+        <img
+          v-if="averageData.arrowImg"
+          class="stat__average-arrow"
+          :src="averageData.arrowImg"
+          alt=""
+          width="15"
+          height="16"
+        />
+        <p class="stat__average-info-descr" :style="{ color: 'var(--neutral-900)' }">
+          {{ averageData.descr }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
